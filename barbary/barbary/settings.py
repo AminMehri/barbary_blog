9898f7b1402512427ca671bb6ef73860e3cc9e21
Aminmehri <aminmehri45@gmail.com>
2022-08-01 10:16:23 +0430

    delete model

--- barbary/barbary/settings.py
+++ barbary/barbary/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'blog',
     'ckeditor',
-    'jalali_date',
-    'django_social_share',
 ]
 
 MIDDLEWARE = [
@@ -134,29 +132,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = "uploads/"
-
-
-# default settings
-JALALI_DATE_DEFAULTS = {
-   'Strftime': {
-        'date': '%y/%m/%d',
-        'datetime': '%H:%M:%S _ %y/%m/%d',
-    },
-    'Static': {
-        'js': [
-            # loading datepicker
-            'admin/js/django_jalali.min.js',
-            # OR
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
-            # 'admin/js/main.js',
-        ],
-        'css': {
-            'all': [
-                'admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css',
-            ]
-        }
-    },
-}
--- barbary/blog/views.py
+++ barbary/blog/views.py
@@ -1,58 +1,13 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from .models import Article
-from django.core.paginator import Paginator
-
-
 
 def home(request):
-    art = Article.objects.filter(status='p')[:5]
 
-    context = {
-        "art": art,
-    }
-    return render(request, 'index.html', context)
-
+    return render(request, 'index.html')
 
 
 def about(request):
-    art = Article.objects.filter(status='p')[:5]
 
-    context = {
-        "art": art,
-    }
-    return render(request, 'about.html', context)
-
-
-
-def news(request, page=1):
-    article_list = Article.objects.filter(status='p')
-    paginator = Paginator(article_list, 6)
-    articles = paginator.get_page(page)
-
-
-    art = Article.objects.filter(status='p')[:5]
-
-
-    context = {
-        "articles": articles,
-        "art":art,
-    }
-    return render(request, 'news.html', context)
-
-
-
-def detail(request, slug):
-    article = get_object_or_404(Article, slug=slug, status='p')
-
-    art = Article.objects.filter(status='p')[:5]
-
-    context = {
-        "article": article,
-        "art": art,
-    }
-    return render(request, "detail.html", context)
-
+    return render(request, 'about.html')
 
 
 def error_404_view(request, exception):
